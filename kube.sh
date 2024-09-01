b7a368df8d3096b2f31825839ddba657d1e2d298
Vishal Kaushik <dixitji8053@gmail.com>
2024-09-01 19:36:21 +0530

    Add delete and correct edit command

--- kube.sh
+++ kube.sh
@@ -40,13 +40,13 @@ kubenet() {
                 kubenet "cm"
                 ;;
             5)
-                kubectl "ds"
+                kubenet "ds"
                 ;;
             6)
-                kubectl "secrets"
+                kubenet "secrets"
                 ;;
             7)
-                kubectl "certificates"
+                kubenet "certificates"
                 ;;
             *)
                 echo "Invalid option. Please try again."
@@ -86,13 +86,13 @@ kubenet() {
                 kubenet "cm"
                 ;;
             5)
-                kubectl "ds"
+                kubenet "ds"
                 ;;
             6)
-                kubectl "secrets"
+                kubenet "secrets"
                 ;;
             7)
-                kubectl "certificates"
+                kubenet "certificates"
                 ;;
             *)
                 echo "Invalid option. Please try again."
@@ -111,6 +111,7 @@ kubenet() {
     elif [ "$o1" == "nodes" ]; then
         echo "1. get nodes"
         echo "2. describe nodes"
+        echo "3. delete node"
 
         read -p "select option" choice
         case $choice in
@@ -118,8 +119,11 @@ kubenet() {
                 kubectl get no
                 ;;
             2)
-                kubectl describe no $no
+                kubenet "describe no"
                 ;;
+            3)
+                kubenet "delete no"
+                    ;;
             *)
                 echo "Invalid option. Please try again."
                 ;;
@@ -128,21 +132,7 @@ kubenet() {
         IFS=$'\n' read -rd '' -a node <<<"$nodes"
         select no in "${node[@]}"; do
             if [ ${#node[@]} -gt 0 ]; then
-                echo "1. get nodes"
-                echo "2. describe nodes"
-
-                read -p "select option" choice
-                case $choice in
-                    1)
-                        kubectl get no
-                        ;;
-                    2)
-                        kubectl describe no $no
-                        ;;
-                    *)
-                        echo "Invalid option. Please try again."
-                        ;;
-                    esac
+                kubectl $o1 $no
             else
                 echo "No resources found in $ns namespace."
             fi
@@ -169,28 +159,76 @@ kubenet() {
                 kubenet "cm"
                 ;;
             5)
-                kubectl "ds"
+                kubenet "ds"
                 ;;
             6)
-                kubectl "secrets"
+                kubenet "secrets"
                 ;;
             7)
-                kubectl "certificates"
+                kubenet "certificates"
                 ;;
             *)
                 echo "Invalid option. Please try again."
                 ;;
         esac    
         select ns in "${namespace[@]}"; do
-            pod="$(kubectl get po -n $ns | awk 'NR>1 {print $1}')"
+            pod="$(kubectl get $o1 -n $ns | awk 'NR>1 {print $1}')"
             IFS=$'\n' read -rd '' -a po <<<"$pod"
-            if [ ${#po[@]} -gt 0 ]; then
-                kubectl edit $o1 -n $ns
-            else
-                echo "No resources found in $ns namespace."
-            fi
-            break
+            select p in "${po[@]}"; do
+                if [ ${#po[@]} -gt 0 ]; then
+                    kubectl edit $o1 $po -n $ns
+                else
+                    echo "No resources found in $ns namespace."
+                fi
+                break
+            done
         done
+        elif [ "$o1" == "delete" ]; then
+        echo "1. po"
+        echo "2. deploy"
+        echo "3. hpa"
+        echo "4. cm"
+
+        read -p "select option" choice
+        case $choice in
+            1)
+                kubenet "po"
+                ;;
+            2)
+                kubenet "deploy"
+                ;;
+            3)
+                kubenet "hpa"
+                ;;
+            4)
+                kubenet "cm"
+                ;;
+            5)
+                kubenet "ds"
+                ;;
+            6)
+                kubenet "secrets"
+                ;;
+            7)
+                kubenet "certificates"
+                ;;
+            *)
+                echo "Invalid option. Please try again."
+                ;;
+        esac    
+        select ns in "${namespace[@]}"; do
+            pod="$(kubectl get $o1 -n $ns | awk 'NR>1 {print $1}')"
+            IFS=$'\n' read -rd '' -a po <<<"$pod"
+            select p in "${po[@]}"; do
+                if [ ${#po[@]} -gt 0 ]; then
+                    kubectl delete $o1 $po -n $ns
+                else
+                    echo "No resources found in $ns namespace."
+                fi
+                break
+            done
+        done
+
     else
         echo "Nothing"
     fi
@@ -202,7 +240,8 @@ echo "2. logs"
 echo "3. describe"
 echo "4. nodes"
 echo "5. edit"
-echo "6. Exit"
+echo "6. Delete"
+echo "7 exit"
 
 read -p "select option" choice
 
@@ -223,12 +262,12 @@ case $choice in
         kubenet "edit"
         ;;
     6)
+        kubenet "delete"
+        ;;
+    7)
         exit 0
         ;;
     *)
         echo "Invalid option. Please try again."
         ;;
 esac    
-
-
-
